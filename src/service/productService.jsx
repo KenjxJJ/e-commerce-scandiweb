9e6ac8a45018de8e01ac3fbd2c1d57c53ed0c329
KenjxJJ <joelkiggwe94@gmail.com>
2022-05-24 23:12:41 +0300

    Add backend url test

--- src/service/productService.jsx
+++ src/service/productService.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-const USER_API_URL = 'http://localhost/e-commerce-php';
+const USER_API_URL = 'https://e-commerce-backend-php.000webhostapp.com';
+// const USER_API_URL = 'https://localhost/e-commerce-php';
 
 class ProductService {
     
     // Obtain products from database..
     getProducts(){
-        return axios.get(`${USER_API_URL}/read.php`);
+        return axios.get(`${USER_API_URL}/index.php`);
     }
 }
 
